8db693e016ee23888af3b8fd0f0e8f5b66eaf1d9
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-20 01:55:01 +0100

    initial commit

---
+++ src/components/Groups.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="f-groups">
+                        <div class="title">Your Favorite Groups</div>
+                        <div class="groups">
+                        <a href="" class="menu-btn"> <div class="image"><img src="images/w3.jpg"></div><span>W3 School</span></a>
+                        <a href="" class="menu-btn"> <div class="image"><img src="images/profil.bmp"></div><span>Codepen</span></a>
+                        <a href="" class="menu-btn"> <div class="image"><img src="images/stack.png"></div><span>Stackoverflow</span></a>
+                        <a href="" class="menu-btn"> <div class="image"><img src="images/js.png"></div><span>Javascript Developer</span></a>
+                     </div>
+    </div>
+</template>
