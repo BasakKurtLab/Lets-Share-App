be37810558961655db316f58a1c5a584665a36d4
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-20 01:55:24 +0100

    initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+    
+        
+                <div class="menu">
+                    <div class="title">Menu</div>
+                    <div class="menu-left">
+                        
+                            <a href="" class="menu-btn"> <i class="fas fa-home"></i><span>Home</span></a>
+                        
+                            <a href="" class="menu-btn"><i class="fas fa-clipboard"></i><span>Posts</span></a>
+                        
+                            <a href="" class="menu-btn"> <i class="fas fa-envelope"></i><span>Messages</span></a>
+                        
+                            <a href="" class="menu-btn"><i class="fas fa-user-friends"></i><span>Friends</span></a>
+                        
+                            
+                        
+                    </div>
+         </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
