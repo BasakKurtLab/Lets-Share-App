0e372153d2e4c7337b8e4206a9f81bec504e032c
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-20 01:54:54 +0100

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="footer">
+        <span>Created By <a href="https://github.com/BasakKurtLab">Basak Kurt</a> | <span class="far fa-copyright"></span>2021 All rights reserved</span>
+    </div>
+</template>
