5961a58c82ac00867585a321611998996be88e33
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-20 01:56:00 +0100

    initial commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,39 @@
+<template>
+   <div class="posts">
+                    <div class="posts-info">
+                        <div class="image">
+                            <img src="../assets/frau.png">
+                        </div>
+                        <div class="posts-name">
+                            
+                                <div class="name">
+                                    Ayse Acar
+                                    <span>added 1 photo</span></div>
+                                
+
+                            <div class="posts-date">Today at.9.29 AM</div>
+                        </div>
+                        
+                        
+                    </div>
+                    <div class="posts-content">
+                        <div class="posts-title">Travelling to Austria</div>
+                        <div class="posts-image">
+                            <img src="images/freedom.jpg">
+                        </div>
+
+                        <div class="posts-like">
+                            <div class="likes"></div>
+                            <div class="comments"></div>
+                        </div>
+
+
+
+                    </div>
+                    <div class="posts-allcommits">
+
+                    </div>
+                    
+                    
+                </div>
+</template>
