10eb1ac5d759642eeaec9dae59ff67ddf52b0bd1
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-12-21 01:39:07 +0100

    added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,70 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div>
+  <Header/>
+  <main>
+        <div class="max-width">
+            <section class="left">
+                <Menu/>
+                    
+                <Groups/>
+
+            </section>
+            <div class="middle">
+            <section class="middle-top">
+                <Post/>
+            </section>
+
+            <section class="middle-bottom">
+            
+
+                <Posts/>
+                
+
+
+            </section>
+        </div>
+            <div class="right">
+                
+                <section class="right-top">
+                    <Events/>
+                </section>
+                <section class="right-bottom">
+                  <Messages/>
+                </section>
+
+            </div>
+        </div>
+
+    </main>
+    <Footer/>
+</div>
 </template>
+
+<script>
+// @ is an alias to /src
+ import Header from "../components/Header.vue";
+  import Menu from "../components/Menu.vue";
+  import Groups from "../components/Groups.vue";
+  import Post from "../components/Post.vue";
+  import Posts from "../components/Posts.vue";
+  import Events from "../components/Events.vue";
+  import Footer from "../components/Footer.vue";
+  import Messages from "../components/Messages.vue";
+
+export default {
+  name: 'About',
+  components: {
+    Header,
+    Menu,
+    Groups,
+    Post,
+    Posts,
+    Events,
+    Messages,
+    Footer
+  },
+  
+ 
+
+}
+</script>
