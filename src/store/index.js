19b4d7a577a0f112273459ce71fe900b47ce6c46
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2022-01-02 00:55:07 +0100

    editted

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,8 @@ export default new Vuex.Store({
     token: "",
     posts: [],
     looding: "",
-    userposts: []
+    userposts: [],
+    friends: []
   },
   getters:
   {
@@ -37,24 +38,44 @@ export default new Vuex.Store({
     },
     userpostsId(state) {
       return state.userposts.find(el => el.name == "id")
+    },
+    friendsEmail(state) {
+      return state.friends.find(el => el.name == "email")
+    },
+    friendsUsername(state) {
+      return state.friends.find(el => el.name == "username")
+    },
+    friendsImg(state) {
+      return state.friends.find(el => el.name == "img")
+    },
+    friendsId(state) {
+      return state.friends.find(el => el.name == "id")
     }
+
   },
   mutations: {
-    addUserposts(state, payload) {
+    addUserPosts(state, payload) {
       state.userposts.push(payload)
     },
-    updateuserposts(state, payload) {
-      state.userposts.splice(payload.id, 1, { id: payload.id, user: payload.user, content: payload.content, date: payload.date })
+    updateUserPosts(state, payload) {
+      state.userposts.splice(payload.id_a, 1, { id: payload.id, user: payload.user, content: payload.content, date: payload.date })
     },
     addPosts(state, payload) {
       state.posts.push(payload)
     },
     updatePosts(state, payload) {
-      state.posts.splice(payload.id, 1, { id: payload.id, user: payload.user, content: payload.content, date: payload.date })
+      state.posts.splice(payload.id_a, 1, { id: payload.id, user: payload.user, content: payload.content, date: payload.date })
+    },
+    addFriends(state, payload) {
+      state.friends.push(payload)
+    },
+    updateFriends(state, payload) {
+      state.friends.splice(payload.id_a, 1, { id: payload.id, username: payload.username, email: payload.email, img: payload.img })
     }
+
   },
   actions: {
-    async inituserPosts(context) {
+    async initUserPosts(context) {
       
       let response = await fetch("http://localhost/userposts.php?token="+ (document.cookie.split("=")[1]));
 
@@ -62,49 +83,102 @@ export default new Vuex.Store({
         
        
        
-        
-          let i = 1;
+      let i = 0;  
       for (let p in result)
       {
-            context.commit("addUserposts", { id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user})
-            i++;
+        context.commit("addUserPosts", { id_a: i, id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user })
+        i++;   
           }
           
-        },
+    },
+    
+    
+        async updateUserPosts(context)
+     {
+          let response = await fetch("http://localhost/userposts.php?token="+ (document.cookie.split("=")[1]));
+          let result = await response.json();
+      
+        
+          
+          let i = 0; 
+         for(let p in result)
+       {
+           context.commit("updateUserPosts", { id_a:i, id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user });
+           i++;
+           
+           
+
+        }
+           
+    },
 
     async initPosts(context) {
       
-      let response = await fetch("http://localhost/posts.php");
+      let response = await fetch("http://localhost/posts.php?token="+ (document.cookie.split("=")[1]));
 
       let result = await response.json();
         
        
        
         
-          let i = 1;
+      let i = 0;   
       for (let p in result)
       {
-            context.commit("addPosts", { id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user})
-            i++;
+        context.commit("addPosts", { id_a: i, id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user })
+        i++;  
           }
           
         },
     
         async updatePosts(context)
      {
-          let response = await fetch("http://localhost/posts.php");
+          let response = await fetch("http://localhost/posts.php?token="+ (document.cookie.split("=")[1]));
           let result = await response.json();
       
         
           
-         let i = 0;
+          let i = 0; 
          for(let p in result)
        {
-           context.commit("updatePosts", { id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user });
-           i++
+           context.commit("updatePosts", { id_a: i, id: result[p].id, content: result[p].content, date: result[p].date, user: result[p].user })
+           i++;
            
            
 
+        }
+           
+    },
+    async initFriends(context) {
+      
+      let response = await fetch("http://localhost/friends.php?token="+ (document.cookie.split("=")[1]));
+
+      let result = await response.json();
+        
+       
+       
+      let i = 0; 
+          
+      for (let p in result)
+      {
+        context.commit("addFriends", { id_a: i, id: result[p].id, username: result[p].username, email: result[p].email, img: result[p].img })
+        i++; 
+          }
+          
+        },
+    
+        async updateFriends(context)
+     {
+          let response = await fetch("http://localhost/friends.php?token="+ (document.cookie.split("=")[1]));
+          let result = await response.json();
+      
+        
+          
+          let i = 0; 
+         for(let p in result)
+       {
+           context.commit("updateFriends", { id_a:i, id: result[p].id, username: result[p].username, email: result[p].email, img: result[p].img })
+           i++;
+         
         }
            
        }
