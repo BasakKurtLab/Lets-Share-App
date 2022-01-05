5ac81952fafed7804ace1626f85a0657f1a19392
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2022-01-06 00:45:58 +0100

    post delete button added

--- src/components/Userposts.vue
+++ src/components/Userposts.vue
@@ -29,7 +29,14 @@
           <!-- <div class="posts-image">
                             <img src="images/freedom.jpg">
                 </div> -->
-          <div class="content">{{ p.content }}{{ p.id }}</div>
+          <div class="content">{{ p.content }}{{ p.id }}
+
+
+            
+            
+            
+          </div>
+          <button  @click="del((p.id))"> Delete</button>
 
           <div class="posts-allcommits" v-if="commited == index">
             <input
@@ -54,7 +61,37 @@ export default {
       posts: [],
     };
   },
-  methods: {},
+  methods: {
+    del:function(index) {
+      
+      fetch("http://localhost/del.php?index=" + index,{
+
+        mode: "cors",
+
+      })
+      .then(response => response.json())
+      .then(sonuc => {
+
+        if(sonuc.successful == "1")
+        {
+          
+          this.$store.dispatch("initUserPosts");
+          this.$store.dispatch("updateUserPosts");
+          
+          
+        }
+
+        
+      })
+      .finally(() =>
+      {
+        this.loading = false;
+      })
+
+    }
+
+    },
+
   created() {
     this.$store.dispatch("initUserPosts");
     this.$store.dispatch("updateUserPosts");
@@ -103,5 +140,23 @@ export default {
 <style scoped>
 i {
   cursor: pointer;
+}
+.posts-content button{
+  align-self: flex-end;
+  padding:3px 6px;
+ 
+  margin-top: 5px;
+  border-radius: 6px;
+  outline: none;
+  border:1px solid rgb(182, 179, 179);
+  align-self: flex-end;
+  background-color: rgb(175, 33, 33);
+  color:var(--c3);
+  cursor: pointer;
+  transition: all o.2s;
+}
+.posts-content button:hover{
+  background-color: rgb(110, 23, 23);
+  color:var(--c3);
 }
 </style>
